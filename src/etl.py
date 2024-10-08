986b5bd0a9857f3f4e8942d31738f0f58352d83f
Jose Vilson <contatojosevilson@gmail.com>
2024-10-08 18:00:53 -0300

    ajustes

--- src/etl.py
+++ src/etl.py
@@ -3,6 +3,8 @@ import pandas as pd
 import glob
 import chardet
 
+
+
 class BcbCosif:
     def __init__(self) -> None:
         self.dtypes_config = {
@@ -15,23 +17,35 @@ class BcbCosif:
                                 'COD_CONGL': str, 
                                 'NOME_CONGL': str, 
                                 'TAXONOMIA': str, 
-                                'CONTA': str, 
+                                'CONTA': int, 
                                 'NOME_CONTA': str, 
                                 'SALDO': 'float64'
                             }
+        self.layout_colunas = ["DATA_BASE", "CNPJ", "NOME_INSTITUICAO", "DOCUMENTO", "CONTA", "NOME_CONTA", "SALDO"]
 
 
     def ler_arquivo(self, arquivo_name: str) -> pd.DataFrame:
         try:
             df = pd.read_csv(arquivo_name, decimal=",", dtype=self.dtypes_config, encoding=self.encoding_file, skiprows=3, sep=";")
-            df.columns = df.columns.str.strip()
+            df.columns = df.columns.str.strip().str.upper()
             self.df = df
 
         except UnicodeDecodeError:
             # Tentativa de leitura com 'iso-8859-1'
             df = pd.read_csv(arquivo_name, decimal=",", dtype=self.dtypes_config, encoding='utf-8',  skiprows=3, sep=";")
-            df.columns = df.columns.strip()
+            df.columns = df.columns.strip().str.upper()
             self.df = df
+
+    def padronizar_colunas(self):
+        mapeamento_colunas = {
+        '#DATA_BASE': 'DATA_BASE', 
+        'DATA': 'DATA_BASE',
+        'NOME INSTITUICAO': 'NOME_INSTITUICAO',
+        'NOME CONTA': 'NOME_CONTA'
+        }
+
+        self.df.rename(columns=mapeamento_colunas, inplace=True)
+        self.df = self.df[self.layout_colunas] 
     
     def save_df(self, path_to_save: str) -> None:
         self.df.to_csv(path_to_save, index=False, sep=';', decimal=',', encoding='utf-8')
@@ -62,14 +76,15 @@ class BcbCosif:
                 arquivo.seek(0)
                 arquivo.write(novo_conteudo)
 
-# %%
 if __name__ == "__main__":
-    lista = glob.glob('../data/*1995*')
+    lista = glob.glob('../data/**')
+    lista.sort()
+    
     for arquivo in lista:
         print(f'filename: {arquivo}')
         f = BcbCosif()
         f._get_encoding(arquivo)
-        f._replace_cabecalho(arquivo)
+        # f._replace_cabecalho(arquivo)
         f.ler_arquivo(arquivo_name=arquivo)
+        f.padronizar_colunas()
         f.save_df(f'../output/{arquivo.split('/')[-1]}')
-# %%
